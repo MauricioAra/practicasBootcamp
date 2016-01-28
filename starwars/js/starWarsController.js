4d86d1a4784acf2b64e1d8c18ff36afb51190676
mmullerc <mmullerc@ucenfotec.ac.cr>
2016-01-28 16:54:33 -0600

    controller

--- starwars/js/starWarsController.js
+++ starwars/js/starWarsController.js
@@ -5,22 +5,40 @@ var starWarsCtrl = (function(){
     return character;
   }
 
-  function setTable(index){
-    $('#characters-table').DataTable( {
-        data: starWarsService.getAll(index),
-        columns: [
-          { title: "Name" },
-          { title: "Height" },
-          { title: "Mass" },
-          { title: "Hair Color" },
-          { title: "Skin Color" },
-          { title: "Eye Color" },
-          { title: "Birthday" },
-          { title: "Gender" }
-        ]
-    });
-  }
 
+
+  function setTable(index){
+
+   var characterList = [{name:"pedro",height:"18ocm",mass:"20",hair_color:"brown",
+   skin_color:"white",eye_color:"green",
+   birth_year:"1990",gender:"male"},{name:"pedro",height:"18ocm",mass:"20",hair_color:"brown",
+   skin_color:"white",eye_color:"green",
+   birth_year:"1990",gender:"male"}];
+
+      var content;
+
+      for(i=0; i<characterList.length; i++){
+        content += '<tr>' +
+                        '<td>' + characterList[i].name + '</td>'+
+                        '<td>' + characterList[i].height +'</td>'+
+                        '<td>' + characterList[i].mass + '</td>'+
+                        '<td>' + characterList[i].hair_color + '</td>'+
+                        '<td>' + characterList[i].skin_color + '</td>'+
+                        '<td>' + characterList[i].eye_color + '</td>'+
+                        '<td>' + characterList[i].birth_year + '</td>'+
+                        '<td>' + characterList[i].gender + '</td>'+
+                        '<td>' + '<button id="'+i+'" class="button-view">View</button>' + '</td>'+
+                      '</tr>';
+      }
+      $('#characters-table').append(content);
+
+      $('.button-view').click(function(evt){
+         getOne(evt.target.id);
+         $('#myModal').on('shown.bs.modal', function () {
+           
+         })
+      });
+  }
   function init(){
     setTable(1);
   }
