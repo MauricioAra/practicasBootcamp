421ff9e927ee6f3473cf5a14cad175073272962e
mmullerc <mmullerc@ucenfotec.ac.cr>
2016-01-29 13:29:07 -0600

    paginacion

--- starwars/js/starWarsController.js
+++ starwars/js/starWarsController.js
@@ -1,15 +1,38 @@
 var starWarsCtrl = (function(){
 
+  var page = 1;
+
+  $('#previous-page').click(function(evt){
+    previousPage();
+  })
+  $('#next-page').click(function(evt){
+    nextPage();
+  });
+
   function getOne(id){
     var character = starWarsApi.getOne(id);
     return character;
   }
 
+  function nextPage(){
+    page ++;
+    setTable(page);
+  }
+  function previousPage(index){
+    page --;
+    if(page == 1){
+      alert("No hay menos paginas!");
+    }else{
+    setTable(index);
+    }
+  }
+
   function setTable(index){
 
-      var characterList = starWarsApi.getAll(index);
-      var id;
+   var characterList = starWarsApi.getAll(index);
+
       var content;
+      var id;
       for(i=0; i<characterList.length; i++){
 
         content += '<tr>' +
@@ -21,10 +44,9 @@ var starWarsCtrl = (function(){
                         '<td>' + characterList[i].eye_color + '</td>'+
                         '<td>' + characterList[i].birth_year + '</td>'+
                         '<td>' + characterList[i].gender + '</td>'+
-                        '<td>' + '<button id="'+(id = i + 1 )+'" class="btn btn-success button-view">View</button>' + '</td>'+
+                        '<td>' + '<button id="'+(id = i + 1 )+'" class="button-view">View</button>' + '</td>'+
                       '</tr>';
       }
-
       $('#characters-table').append(content);
 
       $('.button-view').click(function(evt){
@@ -49,6 +71,8 @@ var starWarsCtrl = (function(){
   }
 
   return {
+    nextPage: nextPage,
+    prevPage: previousPage,
     getAll: setTable,
     initialize: init
   };
